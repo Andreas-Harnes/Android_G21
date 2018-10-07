88cc6be90e6cefaa992de415b99737fbed799f88
Fredrik <fredrik.olsvold@gmail.com>
2018-10-07 18:14:25 +0200

    Begynnelse på SideMeny
    Side meny med 3 items. Ingen funksjoner.

--- BudgetApp/app/src/main/java/no/hiof/fredrivo/budgetapp/MainActivity.java
+++ BudgetApp/app/src/main/java/no/hiof/fredrivo/budgetapp/MainActivity.java
@@ -1,13 +1,39 @@
 package no.hiof.fredrivo.budgetapp;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawLayout;
+    private Toolbar toolbar;
+    private NavigationView navView;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawLayout = findViewById(R.id.drawer_layout);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                drawLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
