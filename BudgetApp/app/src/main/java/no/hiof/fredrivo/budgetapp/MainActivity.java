fd0c9fb87fbb67701d0924f57f18120bd226f531
Fredrik <fredrivo@hiof.no>
2018-10-16 11:22:16 +0200

    Push test

--- BudgetApp/app/src/main/java/no/hiof/fredrivo/budgetapp/MainActivity.java
+++ BudgetApp/app/src/main/java/no/hiof/fredrivo/budgetapp/MainActivity.java
@@ -1,6 +1,7 @@
 package no.hiof.fredrivo.budgetapp;
 
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -24,6 +25,31 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawLayout = findViewById(R.id.drawer_layout);
+
+        navView = findViewById(R.id.navigation_view);
+
+        setUpDrawerContent(navView);
+    }
+
+    private void setUpDrawerContent(NavigationView navView){
+        navView.setNavigationItemSelectedListener(
+            new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(MenuItem menuItem){
+                    selectDrawerItem(menuItem);
+                    return true;
+                }
+            });
+
+    }
+
+    public void selectDrawerItem(MenuItem menuItem){
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch (menuItem.getItemId()){
+            case R.id.menu_Item1:
+                fragmentClass = FirstFragment.class;
+        }
     }
 
     @Override
