77fc083913d86b4e74e0e306ebf0083f25798a4c
Ingrid <ingrid.e.dahl@hiof.no>
2018-10-16 11:31:49 +0200

    Lagd profilside

---
+++ BudgetApp/app/src/main/java/no/hiof/fredrivo/budgetapp/Profil.java
@@ -0,0 +1,13 @@
+package no.hiof.fredrivo.budgetapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Profil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil);
+    }
+}
