3c944337793001e8ad4e306fb42a3f5b635dfe95
Fredrik <fredrivo@hiof.no>
2018-10-16 12:11:53 +0200

    Byttet fra FrameLayout til LinearLayout
    Lagt til fragments i menyen

--- BudgetApp/app/src/main/java/no/hiof/fredrivo/budgetapp/MainActivity.java
+++ BudgetApp/app/src/main/java/no/hiof/fredrivo/budgetapp/MainActivity.java
@@ -2,13 +2,16 @@ package no.hiof.fredrivo.budgetapp;
 
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawLayout;
@@ -48,9 +51,36 @@ public class MainActivity extends AppCompatActivity {
         Class fragmentClass;
         switch (menuItem.getItemId()){
             case R.id.menu_Item1:
-                fragmentClass = FirstFragment.class;
+                fragmentClass = InputActivity.class;
+                break;
+            case R.id.menu_Item2:
+                fragmentClass = InputActivity.class;
+                break;
+            case R.id.menu_Item3:
+                fragmentClass = InputActivity.class;
+                break;
+            default:
+                fragmentClass = InputActivity.class;
+
         }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        LayoutInflater inflater = getLayoutInflater();
+        LinearLayout container = findViewById(R.id.content_frame);
+        inflater.inflate(R.layout.activity_main, container);
+
+        menuItem.setChecked(true);
+
+        setTitle(menuItem.getTitle());
+
+        drawLayout.closeDrawers();
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
