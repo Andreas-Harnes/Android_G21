14299ce80f383abcf485b82026b7f1390cf65cb3
Fredrik <fredrik.olsvold@gmail.com>
2018-10-18 17:06:58 +0200

    Lagt til vanlig MainActivity istedet for Drawer
    Oppdatert klassene

--- BudgetApp/app/src/main/java/no/hiof/fredrivo/budgetapp/classes/Profile.java
+++ BudgetApp/app/src/main/java/no/hiof/fredrivo/budgetapp/classes/Profile.java
@@ -1,9 +1,69 @@
 package no.hiof.fredrivo.budgetapp.classes;
 
+import java.util.ArrayList;
+
 public class Profile {
+    private String profileFirstName;
+    private String profileLastName;
     private int incomePerMonth;
+    private int savePerMonth;
+    private int expensesPerMonth;
+    private ArrayList<String> categoryToSave;
 
+    public Profile(String profileFirstName, String profileLastName, int incomePerMonth, int savePerMonth, int expensesPerMonth, ArrayList<String> category) {
+        this.profileFirstName = profileFirstName;
+        this.profileLastName = profileLastName;
+        this.incomePerMonth = incomePerMonth;
+        this.savePerMonth = savePerMonth;
+        this.expensesPerMonth = expensesPerMonth;
+        this.categoryToSave = category;
+    }
 
-    public Profile() {
+    public String getProfileFirstName() {
+        return profileFirstName;
+    }
+
+    public void setProfileFirstName(String profileFirstName) {
+        this.profileFirstName = profileFirstName;
+    }
+
+    public String getProfileLastName() {
+        return profileLastName;
+    }
+
+    public void setProfileLastName(String profileLastName) {
+        this.profileLastName = profileLastName;
+    }
+
+    public int getIncomePerMonth() {
+        return incomePerMonth;
+    }
+
+    public void setIncomePerMonth(int incomePerMonth) {
+        this.incomePerMonth = incomePerMonth;
+    }
+
+    public int getSavePerMonth() {
+        return savePerMonth;
+    }
+
+    public void setSavePerMonth(int savePerMonth) {
+        this.savePerMonth = savePerMonth;
+    }
+
+    public int getExpensesPerMonth() {
+        return expensesPerMonth;
+    }
+
+    public void setExpensesPerMonth(int expensesPerMonth) {
+        this.expensesPerMonth = expensesPerMonth;
+    }
+
+    public ArrayList<String> getCategoryToSave() {
+        return categoryToSave;
+    }
+
+    public void setCategoryToSave(ArrayList<String> categoryToSave) {
+        this.categoryToSave = categoryToSave;
     }
 }
