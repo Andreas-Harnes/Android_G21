d11fc43129fb26a805a0551a44c47d84ca1f85aa
Fahad Said <fahads9612@gmail.com>
2018-10-19 12:27:03 +0200

    Tabview og layout for overordende oversikt

---
+++ BudgetApp/app/src/main/java/no/hiof/fredrivo/budgetapp/week_tab.java
@@ -0,0 +1,29 @@
+package no.hiof.fredrivo.budgetapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class week_tab extends Fragment {
+
+
+    public week_tab() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_week_tab, container, false);
+    }
+
+}
