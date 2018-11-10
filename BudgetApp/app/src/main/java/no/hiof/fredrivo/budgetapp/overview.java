2f8621a6cf6a476c66c60cfaf5c2c87f3ae6feeb
Fahad Said <fahads9612@gmail.com>
2018-11-10 18:27:37 +0100

    It is a miracle!! <3

--- BudgetApp/app/src/main/java/no/hiof/fredrivo/budgetapp/overview.java
+++ BudgetApp/app/src/main/java/no/hiof/fredrivo/budgetapp/overview.java
@@ -101,42 +101,39 @@ public class overview extends AppCompatActivity {
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
 
-       // intentInputActivity = new Intent(getApplicationContext(), InputActivity.class); */
+       intentInputActivity = new Intent(getApplicationContext(), InputActivity.class);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                test(view);
+                startActivity(intentInputActivity);
 
             }
         });
 
-    }
-    public void test(View view){
-        intentInputActivity = new Intent(this, InputActivity.class);
-        startActivity(intentInputActivity);
+
+
+        //setting up tabs with adapter from new class: sectionspageadapter
+        mSectionsPagerAdapter= new SectionsPagerAdapter(getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+        // TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+        // tablayout end
     }
 
-//    @Override
-//    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
-//        super.onPostCreate(savedInstanceState);
-//        drawerToggle.syncState();
-//    }
-//
-//    @Override
-//    public void onConfigurationChanged(Configuration newConfig) {
-//        super.onConfigurationChanged(newConfig);
-//        drawerToggle.onConfigurationChanged(newConfig);
-//    }
-//
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_overview, menu);
-//        return true;
-//    }
+    //viewpager and adapter
+    private void setupViewPager(ViewPager viewPager) {
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new day_tab(),"Day");
+        adapter.addFragment(new week_tab(),"Week");
+        adapter.addFragment(new month_tab(),"Month");
+        viewPager.setAdapter(adapter);
+    }
 
+    // drawer element from fredrik start
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
@@ -171,74 +168,7 @@ public class overview extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-
-     // A placeholder fragment containing a simple view.
-
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            Fragment fragment = null;
-
-            if (getArguments().getInt(ARG_SECTION_NUMBER) == 1){
-                //TODO: bytt til day_tab fragment
-                return inflater.inflate(R.layout.fragment_day_tab, container, false);
-            }
-            if (getArguments().getInt(ARG_SECTION_NUMBER) == 2){
-                //TODO: bytt til week_tab fragment
-                return inflater.inflate(R.layout.fragment_week_tab, container, false);
-            }
-            if (getArguments().getInt(ARG_SECTION_NUMBER) == 3){
-                //TODO: bytt til month_tab fragment
-                return inflater.inflate(R.layout.fragment_month_tab, container, false);
-            }
-            View rootView = inflater.inflate(R.layout.fragment_overview, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
+    // slutt
 
 
-    }
 }
