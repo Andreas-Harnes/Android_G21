1bfc7d5cc07cf052823cbbfde8832d1e19cb6dbb
Fredrik <fredrivo@hiof.no>
2018-11-17 18:41:22 +0100

    Vi har pieChart og sidemeny i activitien
    Bytte farge på spesifik value how?? Moneleft bør være grønn, resten uspesifik farge

--- BudgetApp/app/src/main/java/no/hiof/fredrivo/budgetapp/ChartActivity.java
+++ BudgetApp/app/src/main/java/no/hiof/fredrivo/budgetapp/ChartActivity.java
@@ -1,14 +1,24 @@
 package no.hiof.fredrivo.budgetapp;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
@@ -21,12 +31,27 @@ import java.util.List;
 
 import no.hiof.fredrivo.budgetapp.classes.Expenses;
 
-public class ChartActivity extends AppCompatActivity {
-
+public class ChartActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chart);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        DrawerLayout draw = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,draw,toolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        draw.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         PieChart pieChart = findViewById(R.id.pieChartLayout);
 
@@ -39,29 +64,53 @@ public class ChartActivity extends AppCompatActivity {
         TestData.add(new Expenses(150, "03/11/2017", "Halden", "Mat?","Drikke"));
         TestData.add(new Expenses(400, "17/08/2017", "Halden", "namm","Pølse"));
 
-        for (Expenses values : TestData) {
+        //TODO: Endres til passende verdi fra inntekter/hvor mye penger man har å bruke.
+        int moneyLeft = 1000;
 
+        //Legger til expense objekter som entries som pieChartData
+        for (Expenses values : TestData) {
             pieChartList.add(new PieEntry(values.getSum(), values.getCategory()));
+            moneyLeft = moneyLeft - values.getSum();
+
 
         }
+        PieEntry moneyLeftEntry = new PieEntry(moneyLeft, "Money Left");
+        pieChartList.add(moneyLeftEntry);
 
         PieDataSet dataSet = new PieDataSet(pieChartList, "");
         dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
         dataSet.setValueTextSize(16f);
 
-        //Description
-        Description pieChartDescription = new Description();
-        pieChart.setDescription(pieChartDescription);
-        pieChartDescription.setText("Your total expenses");
-        pieChartDescription.setTextSize(16f);
-        pieChartDescription.setPosition(750, 150);
-
         PieData pieData = new PieData(dataSet);
 
         pieChart.setData(pieData);
+        pieChart.getDescription().setEnabled(false);
         pieChart.invalidate();
 
 
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+
+        if (id == R.id.home) {
+            // Handle the camera action
+        } else if (id == R.id.profile) {
+            Intent intent = new Intent(this, ProfilActivity.class);
+            startActivity(intent);
+
+        } else if (id == R.id.detail) {
+            Intent intent = new Intent(this,DetailActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.chart) {
+            Intent intent = new Intent(this,ChartActivity.class);
+            startActivity(intent);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
