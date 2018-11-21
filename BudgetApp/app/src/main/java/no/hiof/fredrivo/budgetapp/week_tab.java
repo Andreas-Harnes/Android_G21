2e4197b554928558ba6ea94c321a3acdd2074f8c
Andreas Harnes <andreas.harnes@hotmail.no>
2018-11-21 04:15:29 +0100

    Fikset week taben

--- BudgetApp/app/src/main/java/no/hiof/fredrivo/budgetapp/week_tab.java
+++ BudgetApp/app/src/main/java/no/hiof/fredrivo/budgetapp/week_tab.java
@@ -29,6 +29,7 @@ import no.hiof.fredrivo.budgetapp.Adapter.DayTabAdapter;
 import no.hiof.fredrivo.budgetapp.Adapter.WeekTabAdapter;
 import no.hiof.fredrivo.budgetapp.R;
 import no.hiof.fredrivo.budgetapp.classes.Expenses;
+import no.hiof.fredrivo.budgetapp.classes.WeekDates;
 
 public class week_tab extends Fragment {
 
@@ -39,6 +40,8 @@ public class week_tab extends Fragment {
     private GoogleSignInAccount account;
     private TextView txtDaySum;
     private WeekTabAdapter weekTabAdapter;
+    private WeekDates c;
+
 
 
 
@@ -46,9 +49,12 @@ public class week_tab extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
+
         expensesArrayList.clear();
         mDatabaseRef = FirebaseDatabase.getInstance().getReference();
         account = GoogleSignIn.getLastSignedInAccount(getContext());
+        c = new WeekDates();
 
 
         Toast.makeText(getContext(), account.getEmail(), Toast.LENGTH_SHORT).show();
@@ -95,62 +101,14 @@ public class week_tab extends Fragment {
         return root;
     }
 
-    // Sjekker om datoen på expens objektet er fra dags Måned
-    private boolean validData(Expenses data){
-
-        String regex;
-
-        int fBoolean = 0;
-
-        //gets todays date from calendar object
-        Calendar calendar = Calendar.getInstance();
-        int intYear = calendar.get(Calendar.YEAR);
-        int intMonth = calendar.get(Calendar.MONTH) + 1;
-        int intDay = calendar.get(Calendar.DAY_OF_MONTH);
-
-        String day = "";
-        String month = "";
-        String year = "";
-
-
-        // Setter day variabelen
-        if(intDay < 10){
-            day = "0" + String.valueOf(intDay);
-        } else {
-            day = String.valueOf(intDay);
+    // Sjekker om datoen på expens objektet er fra denne uken
+    private boolean validData(Expenses data, WeekDates weekList){
+        for (String x : weekList.getDates()) {
+            if(data.getDate().equals(x)){
+                return  true;
+            }
         }
-
-
-        // Setter month variabelen
-        if(intMonth < 10){
-            month = "0" + String.valueOf(intMonth);
-        } else {
-            month = String.valueOf(intMonth);
-        }
-
-        year = String.valueOf(intYear);
-
-        // TODO: Ma fikses slik at den viser til denne måneden
-
-        regex = "((.)(.))(/)(("
-                + month.substring(0,1) + ")(" + month.substring(1) +
-                "))(/)((" + year.substring(0,1) + ")(" + year.substring(1,2) + ")(" +
-                year.substring(2,3) + ")(" + year.substring(3) + "))";
-
-//        Toast.makeText(getContext(), data.getDate(), Toast.LENGTH_SHORT).show();
-
-        if(data.getDate().matches(regex)){
-            fBoolean = 1;
-        }
-
-        if(fBoolean == 1){
-            return true;
-        } else {
-            return false;
-        }
-
-        //Toast.makeText(getContext(), data, Toast.LENGTH_LONG).show();
-
+        return false;
     }
 
     private void showData(DataSnapshot dataSnapshot) {
@@ -158,9 +116,7 @@ public class week_tab extends Fragment {
         weekCategoryList.clear();
         for(DataSnapshot ds : dataSnapshot.child(account.getId()).child("Expenses").getChildren()) {
 
-            Expenses x = ds.getValue(Expenses.class);
-
-            if(validData(ds.getValue(Expenses.class))){
+            if(validData(ds.getValue(Expenses.class), c)){
                 Expenses userExpense = new Expenses();
                 userExpense.setSum(ds.getValue(Expenses.class).getSum());
                 userExpense.setDate(ds.getValue(Expenses.class).getDate());
